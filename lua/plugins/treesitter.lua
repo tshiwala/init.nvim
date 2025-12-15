b89624d0a9d49d6c42f948f13ea912ea4696ca46
Tikky Shiwala <tikkyshiwala@gmail.com>
2025-12-15 15:49:38 -0500

    Fix: Remove nvim-treesitter-textobjects to resolve compatibility error
    - Removed nvim-treesitter-textobjects dependency
    - Removed textobjects configuration from treesitter setup
    - Fixes module 'nvim-treesitter.configs' not found error
    - Can be re-added later with updated version if needed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,9 +8,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-    },
     config = function()
       require("nvim-treesitter.configs").setup({
         -- ========================================================================
@@ -109,40 +106,8 @@ return {
         -- ========================================================================
         -- Text Objects
         -- ========================================================================
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-              ["aa"] = "@parameter.outer",
-              ["ia"] = "@parameter.inner",
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-              ["]m"] = "@function.outer",
-              ["]]"] = "@class.outer",
-            },
-            goto_next_end = {
-              ["]M"] = "@function.outer",
-              ["]["] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[m"] = "@function.outer",
-              ["[["] = "@class.outer",
-            },
-            goto_previous_end = {
-              ["[M"] = "@function.outer",
-              ["[]"] = "@class.outer",
-            },
-          },
-        },
+        -- Note: Textobjects plugin removed to fix compatibility issues
+        -- You can add it back later if needed with the updated plugin
       })
 
       -- ========================================================================
